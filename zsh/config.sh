e5a0a8824764471c20531a42b9097830b0c50a2e
Giuseppe Lillo <giuseppe.lillo8@gmail.com>
2020-06-10 10:42:56 +0200

    Add zsh config

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,5 +1,5 @@
-set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
-set -gx SBT_OPTS "-XX:+CMSClassUnloadingEnabled -Xmx2G"
-set -gx VISUAL vim
-set -gx EDITOR "$VISUAL"
-
+#set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
+#set -gx SBT_OPTS "-XX:+CMSClassUnloadingEnabled -Xmx2G"
+#set -gx VISUAL vim
+#set -gx EDITOR "$VISUAL"
+alias ll="ls -alF"
